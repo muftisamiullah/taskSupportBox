41dbef0711e881f833e6adec78325146c9a8ebe7
Sami Ullah <muftisami2010@gmail.com>
2018-12-23 23:21:48 +0530

    initial

---
+++ index.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="FontsFree-Net-Proxima-Nova-Sbold.otf" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css" type="text/css">
+    
+        <title>Eventackle - HOME</title>
+  </head>
+<body>
+<nav class="navbar my-nav navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#">
+    <img src="images/logo.svg" width="30" height="30" class="d-inline-block align-top logo" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+  </button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="nav navbar-nav ml-auto pt-1">
+    <li class="nav-item">
+      <button class="btn-sm btn-s"><strong>Create Event</strong></button>
+    </li>
+    <li class="nav-item">
+    <button class="btn btn-outline btn-sm btn-l"><strong>Log In</strong></button>
+    </li>
+</div>
+</nav>
+
+<div class="wrapper">
+  <div class="inner">
+      <div class="content">
+          <h4 class="wow fadeInDown" data-wow-delay="1s">Bringing all the people together through live experiences!</h4>
+          <div class="row ">
+            <div class="col-sm-3 mx-auto">
+              <div class="input-group mycustom md-3">
+                <input type="text" class="form-control" placeholder="Search events or categories" aria-describedby="button-addon2">
+                  <div class="input-group-prepend">
+                    <button class="wow bounceInLeft btn-lg btn-search">Search</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+<div class="events">
+  <strong><p class="p-up" style="display:inline;">Events for you in</p> <p class="p-upp" style="display:inline;">Srinagar, India</p><strong>  <img class="pen" src="images/ic-mode-edit-18-px.svg"></img>
+</div><br>
+
+<!-- <div class="container"> -->
+  <div class="row justify-content-center align-self-center">
+    <div class="col-xs-2">
+      <div class="card mb-4 button-wrapper " style="width: 18rem;">
+        <img class="card-img-top" src="images/ist.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Travelling Perks</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>Creativity and Marketing</p>
+          <hr>
+          <img src="images/wishlist.svg"></img>
+          <a href="" class="btn btn-outline ptags">Add to Wishlist</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-2">
+      <div class="card mb-4 button-wrapper" style="width: 18rem;">
+        <img class="card-img-top" src="images/2nd.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Ancient Architecture</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>History</p>
+          <hr>
+          <img src="images/wishlist.svg"></img>
+          <a href="#" class="btn btn-outline ptags">Add to Wishlist</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-2">
+      <div class="card mb-4 button-wrapper" style="width: 18rem;">
+        <img class="card-img-top" src="images/3rd.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Adrenaline Rush</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>History</p>
+          <hr>
+          <img src="images/wishlist.svg"></img>
+          <a href="#" class="btn btn-outline ptags">Add to Wishlist</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-2">
+      <div class="card button-wrapper" style="width: 18rem;">
+        <img class="card-img-top" src="images/second.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Long Paths</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>Life and Goals</p>
+          <hr>
+          <img src="images/wishlist.svg"></img>
+          <a href="#" class="btn btn-outline ptags">Add to Wishlist</a>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- </div> -->
+
+<div class="container">
+  <p class="p-up" style="text-align:center;">Featured Events</p><br>
+  <div class="row justify-content-center align-self-center">
+    <div class="col-xs-3">
+      <div class="card mb-4" style="width: 18rem;">
+        <img class="card-img-top" src="images/first.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Creative Corner</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>Creativity and Marketing</p>
+          <!-- <hr> -->
+          <!-- <a href="#" class="btn btn-outline">Add to Wishlist</a> -->
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-1">
+    </div>
+    <div class="col-xs-3">
+      <div class="card mb-4" style="width: 18rem;">
+        <img class="card-img-top" src="images/second.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">What made them lead..</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>History</p>
+          <!-- <hr> -->
+          <!-- <a href="#" class="btn btn-outline">Add to Wishlist</a> -->
+        </div>
+      </div>
+    </div><div class="col-sm-1">
+    </div>
+    <div class="col-xs-3">
+      <div class="card mb-4" style="width: 18rem;">
+        <img class="card-img-top" src="images/third.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title ptagsss">Long Paths</h5>
+          <p class="card-text ptags">Monday, April 10, 6:00 PM<br><br>Life and Goals</p>
+          <!-- <hr> -->
+          <!-- <a href="#" class="btn btn-outline">Add to Wishlist</a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <p style="color:#1aa2c5;">Show all (112)</p>
+</div>
+
+
+<div class="container">
+  <p class="p-up" style="text-align:center;">Event Categories</p><br>
+  <div class="row">
+    <div class="col-sm-7">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/oil.jpg">
+        <div class="caption">
+            <p>Oil and Gas</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-5">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/mining.jpg">
+        <div class="caption">
+            <p>Mining</p>
+        </div>
+      </div> 
+    </div>
+  </div> <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/energy.jpg">
+        <div class="caption">
+            <p>Renewable Energy</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/health.jpg">
+        <div class="caption">
+            <p>Health Care</p>
+        </div>
+      </div> 
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/air.jpg">
+        <div class="caption">
+            <p>Aviation</p>
+        </div>
+      </div> 
+    </div>
+  </div>   
+  <br>
+  <div class="row">
+    <div class="col-sm-5">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/dance.jpg">
+        <div class="caption">
+            <p>Music</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-7">
+      <div class="thumbnail text-center">
+        <img class="responsive" src="images/color.jpg">
+        <div class="caption">
+            <p>Creativity</p>
+        </div>
+      </div> 
+    </div>
+  </div><br>
+  <p style="color:#1aa2c5;">Show all (12)</p>
+</div> <br>
+
+
+<div class="jumbo">
+  <div class="container">
+    <strong><h3 class="p-up"style="text-align:center;">How Eventackle Works</h3></strong>
+      <div class="row">
+        <div class="col-sm-6">
+          <span>
+            <strong><p class="ptagsss">Find Events</p></strong>
+              <p class="ptags">Discover corporate events from across the globe!</p>
+              <strong><p class="org ptags"  style="display:inline;">Find Events</p></strong> <img class="arrow" style="display:inline;" src="images/ic-arrow-downward-24-px.svg"></img>
+            </span>
+        </div>
+      <div class="col-sm-6">
+
+      <strong><p class="ptagsss">Create Event</p></strong>
+      <p class="ptags">Create beautiful event listing pages.</p>
+      <strong><p class="org ptags" style="display:inline;">Create Events</p></strong> <img class="arrow" style="display:inline;" src="images/ic-arrow-downward-24-px.svg"></img>
+      </div>
+   </div>
+  </div>
+</div>
+
+<br>
+<br>
+
+<div class="contain">
+  <div class="row">
+    <div class="col-sm-1">
+    </div>   
+    <div class="col-xs-2">
+      <div class="cards" style="width: 18rem;">
+      <strong><p class="ptagss">Use Eventackle</p></strong>
+      <p class="ptags">How it works</p>
+      <p class="ptags">FAQ</p>
+      <p class="ptags">Sitemap</p>
+      </div>
+    </div>
+    <div class="col-sm-1">
+    </div>   
+    <!-- <div class="col-sm-1">
+    </div> -->
+    <div class="col-xs-2">
+      <div class="cards " style="width: 16rem;">
+        <strong><p class="ptagss">For Organisers</p></strong>
+        <p class="ptags">List your event</p>
+        <p class="ptags">Testimonials</p>
+        <p class="ptags">Benefits</p>
+        </div>  
+      </div>
+      <div class="col-sm-1">
+    </div> 
+    <!-- <div class="col-sm-1">
+    </div>  -->
+    <div class="col-xs-2">
+     <div class="cards" style="width: 16rem;">
+        <strong><p class="ptagss">Events</p></strong>
+        <p class="ptags">About Us</p>
+        <p class="ptags">Careers</p>
+        <p class="ptags">Blog</p>
+        </div>
+      </div>
+      <div class="col-sm-1">
+    </div> 
+    <!-- <div class="col-sm-1">
+    </div>  -->
+    <div class="col-xs-2">
+      <div class="cards" style="width: 16rem;"> 
+        <strong><p class="ptagss">Social</p></strong>
+        <p class="ptags"><img class="facebook" src="images/facebook.svg"></img> Facebook</p>
+        <p class="ptags"><img class="facebook" src="images/twitter.svg"></img> Twitter</p>
+        <p class="ptags"><img class="facebook" src="images/instagram.svg"></img> Instagram</p>
+      </div>
+    </div> 
+  </div>
+</div>
+<br>
+<br>
+
+<div class="footer">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light Rectangle-7">
+  <a class="navbar-brand" href="#">
+    <img src="images/logo-copy.svg" width="30" height="30" class="d-inline-block align-top logo" alt="">
+  </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto footer-ul">
+      <li class="nav-item">
+          <a class="nav-link footer-a">© 2018, Eventackle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link footer-a" href="">Cookies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link footer-a" href="">Terms & Privacy</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link footer-a" href="http://muftisamiullah.co.nf">Designed by: muftisamiullah</a>
+        </li>
+      </ul>        
+    </div>
+  </nav>
