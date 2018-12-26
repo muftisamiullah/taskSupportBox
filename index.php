704f45c94feab26f00e635c8c0ca2a127983d280
Sami Ullah <muftisami2010@gmail.com>
2018-12-26 16:24:04 +0530

    half way through making the design responsive

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
     <div class=row4>
       <div class="container">
         <div class="row">
-          <div class="col-sm-4"><br>
+          <div class="col-lg-4 col-md-4 col-sm-push-12 col-xs-push-12"><br>
             <img src="/images/logo.png" class="d-inline-block align-top" alt="">
           </div>
-          <div class="col-sm-8">
+          <div class="col-lg-8 col-md-8 col-sm-pull-12 col-xs-pull-12">
             <div class="top-bar">
                 <a href="" class="e-mail">info@albawadimetals.com</a>
                 <ul class="social css3 display">
@@ -32,33 +32,33 @@
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              
-            <ul class="navbar-nav my ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">ABOUT US</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PRODUCTS</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">CONTACT US</a>
-            </li>
-            </ul>
+              <ul class="navbar-nav my ml-auto" >
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ABOUT US</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PRODUCTS</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT US</a>
+              </li>
+              </ul>
             </div>
-          </nav>
+            </nav>
           </div>
-        </div>
-      </div>  
+        </div>  
+      </div>
     </div>
+
     <div id="carouselExampleSlides" class="carousel slide">
       <div class="carousel-inner">
         <div class="carousel-item active">
@@ -84,35 +84,35 @@
     <div class="row3"><br>
       <div class="container">
         <h2>Our Products</h2>
-        <div class="heading-holder"></div>
+          <div class="heading-holder"></div>
           <div class="row">
-            <div class="col-sm-3">
-              <img class="imgs3" src="images/rolling-warehouse-aluminium-ladder.jpg">
-              <a class="apro" href="#">
-              <figure class="product-icons ladders"></figure>Aluminium Scaffolding
-              </a>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-2">
+              <img class="rounded-circle img-fluid" src="images/rolling-warehouse-aluminium-ladder.jpg"><br>
+                <!-- <a href="#"> -->
+                <h5 class="head6">Aluminium Scaffolding</h5>
+                <!-- </a> -->
             </div>
-          <div class="col-sm-3">
-              <img class="imgs3" src="images/aluminium-podium-scaffolding.jpg">
-              <a class="apro" href="#">
-              <figure class="product-icons ladders"></figure>Aluminium Ladders
-              </a>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-2">
+                <img class="rounded-circle img-fluid" src="images/aluminium-podium-scaffolding.jpg">
+                <a href="#">
+                <h5 class="head6">Aluminium Ladders</h5>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <img class="rounded-circle img-fluid" src="images/aluminium-mobile-narrow-scaffolding.jpg">
+                <a href="#">
+                <h5 class="head6">Steel Cots</h5>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <img class="rounded-circle img-fluid" src="images/aluminium-folding-scaffolding.jpg">
+                <a href="#">
+                <h5 class="head6">Steel Furniture</h5>
+                </a>
+            </div>
           </div>
-          <div class="col-sm-3">
-              <img class="imgs3" src="images/aluminium-mobile-narrow-scaffolding.jpg">
-              <a class="apro" href="#">
-              <figure class="product-icons ladders"></figure>Steel Cots
-              </a>
-          </div>
-          <div class="col-sm-3">
-              <img class="imgs3" src="images/aluminium-folding-scaffolding.jpg">
-              <a class="apro" href="#">
-              <figure class="product-icons ladders"></figure>Steel Furniture
-              </a>
-          </div>
-        </div>
-      </div><br><br><br>
-    </div>
+      </div>
+    </div><br><br>
 
     <div class="row2"><br>
       <div class="container">
@@ -191,16 +191,16 @@
     <div class="row1">
       <div class="container"><br>
         <div class="row">
-          <div class="col-sm-3">
-            <h5>Aluminium Scaffolding</h5><hr class="newhr">
+          <div class="col-lg-3 col-md-3 text-center">
+            <h5 class="head5">Aluminium Scaffolding</h5><hr class="newhr">
             <ul><li><a class="fli" href="#"> Mobile Wide Scaffolding</a></li>
               <li><a class="fli" href="#"> Mobile Narrow Scaffolding</a></a></li>
                 <li><a class="fli" href="#"> Folding Scaffolding</a></a></li>
                 <li><a class="fli" href="#"> Podium Scaffolding</a></a></li>
             </ul>
           </div>
-          <div class="col-sm-3">
-            <h5>Aluminium Ladders</h5><hr class="newhr">
+          <div class="col-lg-3 col-md-3 text-center">
+            <h5 class="head5">Aluminium Ladders</h5><hr class="newhr">
             <ul><li><a class="fli" href="#"> Platform Step Ladder</a></a></li>
                 <li><a class="fli" href="#"> Rolling Warehouse Ladder</a></a></li>
                 <li><a class="fli" href="#"> Two Way Ladder</a></a></li>
@@ -210,8 +210,8 @@
                 <li><a class="fli" href="#">Multi Task Ladder</a></li>
             </ul>
           </div>
-          <div class="col-sm-3">
-            <h5>Steel Cots</h5><hr class="newhr">
+          <div class="col-lg-3 col-md-3 text-center">
+            <h5 class="head5">Steel Cots</h5><hr class="newhr">
             <ul><li><a class="fli" href="#"> Single Cot Top 110 S</a></a></li>
                 <li><a class="fli" href="#"> Single Cot Top 210 S</a></a></li>
                 <li><a class="fli" href="#"> Single Cot Top 310 S</a></li>
@@ -219,8 +219,8 @@
                 <li><a class="fli" href="#"> Detachable Cot 210 B</a></li>
             </ul>
           </div>
-          <div class="col-sm-3">
-            <h5>Steel Furniture</h5><hr class="newhr">
+          <div class="col-lg-3 col-md-3 text-center">
+            <h5 class="head5">Steel Furniture</h5><hr class="newhr">
             <ul><li><a class="fli" href="#"> Steel Shoe Rack</a></li>
                 <li><a class="fli" href="#"> Steel Lockers</a></li>
                 <li><a class="fli" href="#"> Steel Vertical Filing Cabinet</a></li>
@@ -228,15 +228,21 @@
             </ul>
           </div>
         </div>
-      </div><br>
+      </div>
     </div>
-    <footer>
+    <!-- <footer> -->
+    <div class="footdiv"><br>
       <div class="container">
-        <div class="footdiv">
-          <p class="fp">Copyright &copy; 2018. Al Bawadi Metal Industries LLC, All Rights Reserved.</p>
-          <p class="fp1">Design by: <a class="web" href="http://muftisamiullah.co.nf">Sami Ullah</a></p>
+        <div class="row">
+          <div class="col-lg-10 col-md-9 col-sm-8 col-xs-9 text-center">
+            <p class="fp">Copyright &copy; 2018. Al Bawadi Metal Industries LLC, All Rights Reserved.</p>
+          </div>
+          <div class="col-lg-2 col-md-3 col-sm-4 col-xs-3 text-center">
+            <p class="fp1">Design by: <a class="web" href="http://muftisamiullah.co.nf">Sami Ullah</a></p>
+          </div>
         </div>
       </div>
-    </footer>
+    </div>
+    <!-- </footer> -->
   </body>
 </html>
